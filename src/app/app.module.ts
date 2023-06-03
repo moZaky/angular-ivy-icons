c587ca517e80826aa129e3713297ef6cb007a4a2
Mohammed Elkassed <mohammed.elkassed@maqta.ae>
2023-06-03 20:55:00 +0400

    new icons app

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconService } from './services/icon.service';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MatSelectModule,
+    ReactiveFormsModule
+  ],
+  providers: [IconService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
